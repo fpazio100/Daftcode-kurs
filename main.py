be20d5693613cf92e6b03b05e525c2aefd331eff
fpazio100 <56295969+fpazio100@users.noreply.github.com>
2021-04-20 01:13:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class HelloResp(BaseModel):
 
 @app.get("/")
 def root_view():
-    return {"message": "Hello World"}
+    return {"message": "Hello world!"}
 
 
 @app.get("/hello/{name}", response_model=HelloResp)
@@ -23,3 +23,28 @@ def hello_name_view(name: str):
 def counter():
     app.counter += 1
     return str(app.counter)
+
+
+@app.get("/method", status_code=200)
+def method_g():
+    return {"message": "GET"}
+
+
+@app.put("/method", status_code=200)
+def method_p():
+    return {"message": "PUT"}
+
+
+@app.options("/method", status_code=200)
+def method_o():
+    return {"message": "OPTIONS"}
+
+
+@app.delete("/method", status_code=200)
+def method_d():
+    return {"message": "DELETE"}
+
+
+@app.post("/method", status_code=201)
+def method_po():
+    return {"message": "POST"}
