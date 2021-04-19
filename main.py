877d9fa71e9e97e639b12384555152f3de3e7c93
fpazio100 <56295969+fpazio100@users.noreply.github.com>
2021-04-19 22:08:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 app.counter = 0
+
+
+class HelloResp(BaseModel):
+    msg: str
 
 
 @app.get("/")
@@ -9,12 +14,12 @@ def root_view():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
+@app.get("/hello/{name}", response_model=HelloResp)
 def hello_name_view(name: str):
-    return f"Hello {name}"
+    return HelloResp(msg=f"Hello {name}")
 
 
 @app.get("/counter")
 def counter():
     app.counter += 1
-    return app.counter
+    return str(app.counter)
