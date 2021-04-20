8a98ccea91d50651f738859a220a51b7dc980f1f
fpazio100 <56295969+fpazio100@users.noreply.github.com>
2021-04-20 19:04:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import hashlib
+import datetime
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 app.counter = 0
+app.id = 0
 
 
 class Message(BaseModel):
     message: str
+
+
+class PatientIn(BaseModel):
+    name: str
+    surname: str
 
 
 @app.get("/", status_code=200)
@@ -54,5 +62,10 @@ def method_po():
 @app.get("/auth", status_code=204)
 def haslo_hash_no(password: str, password_hash: str):
     p = hashlib.sha512(password.encode('utf-8')).hexdigest()
-    if password_hash == "" or password == "" or password_hash != p :
+    if password_hash == "" or password == "" or password_hash != p:
         raise HTTPException(status_code=401)
+
+
+@app.post("/register", status_code=201, response_model=PatientIn)
+def register_pac(patient: PatientIn):
+    return PatientIn(patient)
